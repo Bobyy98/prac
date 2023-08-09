255ba99579bc92b6211d2f35f19e7101e9602f51
Bobyy98 <132492809+Bobyy98@users.noreply.github.com>
2023-08-10 00:11:35 +0530

    Create ComplexNoCalcVar2.java

---
+++ Java prog/Complex Calculator (Two Classes)/ComplexNoCalcVar2.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+class ComplexNo {
+    double real;
+    double imag;
+
+    ComplexNo(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    ComplexNo add(ComplexNo other) {
+        double newReal = this.real + other.real;
+        double newImag = this.imag + other.imag;
+        return new ComplexNo(newReal, newImag);
+    }
+
+    ComplexNo sub(ComplexNo other) {
+        double newReal = this.real - other.real;
+        double newImag = this.imag - other.imag;
+        return new ComplexNo(newReal, newImag);
+    }
+
+    ComplexNo mul(ComplexNo other) {
+        double newReal = this.real * other.real - this.imag * other.imag;
+        double newImag = this.real * other.real + this.imag * other.real;
+        return new ComplexNo(newReal, newImag);
+    }
+
+    ComplexNo div(ComplexNo other) {
+        double den = other.real * other.real + other.imag * other.imag;
+        double newReal = (this.real * other.real + this.imag * other.imag) / den;
+        double newImag = (this.imag * other.real - this.real * other.imag) / den;
+        return new ComplexNo(newReal, newImag);
+    }
+
+    void display() {
+        if (imag >= 0) {
+            System.out.println(real + " + " + imag + "i");
+        } else {
+            System.out.println(real + " - " + (-imag) + "i");
+        }
+    }
+}
+
+public class ComplexNoCalcVar2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 1st complex number (real imaginary): ");
+        double real1 = scanner.nextDouble();
+        double imag1 = scanner.nextDouble();
+        ComplexNo num1 = new ComplexNo(real1, imag1);
+
+        System.out.println("Enter 2nd complex number (real imaginary): ");
+        double real2 = scanner.nextDouble();
+        double imag2 = scanner.nextDouble();
+        ComplexNo num2 = new ComplexNo(real2, imag2);
+
+        ComplexNo sum = num1.add(num2);
+        ComplexNo diff = num1.sub(num2);
+        ComplexNo prod = num1.mul(num2);
+        ComplexNo quo = num1.div(num2);
+
+        System.out.print("\n1st complex number: ");
+        num1.display();
+
+        System.out.print("2nd complex number: ");
+        num2.display();
+
+        System.out.println("\nAddition: ");
+        sum.display();
+
+        System.out.println("\nSubtraction: ");
+        diff.display();
+
+        System.out.println("\nMultiplication: ");
+        prod.display();
+
+        System.out.println("\nDivision: ");
+        quo.display();
+
+        scanner.close();
+    }
+}
