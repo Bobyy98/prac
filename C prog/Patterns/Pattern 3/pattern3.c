a3cf65a8d253ec7d1346949176e4f43ba1b34687
Bobyy98 <132492809+Bobyy98@users.noreply.github.com>
2023-08-11 00:10:21 +0530

    Create pattern3.c

---
+++ C prog/Patterns/Pattern 3/pattern3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+    int r, i, j;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &r);
+
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < r; j++)
+        {
+            if (i == j)
+            {
+                printf("1\t");
+            }
+            else
+            {
+                printf("0\t");
+            }
+        }
+        printf("\n");
+    }
+}
