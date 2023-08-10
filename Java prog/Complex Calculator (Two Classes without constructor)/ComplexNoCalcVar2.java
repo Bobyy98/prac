357cf5b748c85c558eef1a11ba63c2a58f2ae7c4
Bobyy98 <132492809+Bobyy98@users.noreply.github.com>
2023-08-10 23:37:23 +0530

    Update ComplexNoCalcVar2.java

--- Java prog/Complex Calculator (Two Classes without constructor)/ComplexNoCalcVar2.java
+++ Java prog/Complex Calculator (Two Classes without constructor)/ComplexNoCalcVar2.java
@@ -1,73 +1,72 @@
 import java.util.Scanner;
 
-class meth 
-{
+class Meth {
     double r1, r2, i1, i2;
 
     void add() 
     {
         double rsum = r1 + r2;
         double isum = i1 + i2;
-        if(isum >= 0)
+        if (isum >= 0) 
         {
             System.out.println("\nSum is " + rsum + " + " + isum + "i");
-        }
-        else
+        } 
+        else 
         {
             System.out.println("Sum is " + rsum + " - " + (-isum) + "i");
         }
     }
 
-    void sub()
+    void sub() 
     {
         double rsub = r1 - r2;
         double isub = i1 - i2;
-        if(isub >= 0)
+        if (isub >= 0) 
         {
             System.out.println("Difference is " + rsub + " + " + isub + "i");
-        }
-        else
+        } 
+        else 
         {
             System.out.println("Difference is " + rsub + " - " + (-isub) + "i");
         }
     }
 
-    void mul()
+    void mul() 
     {
         double rmul = r1 * r2 - i1 * i2;
         double imul = r1 * r2 + i1 * i2;
-        if(imul >= 0)
+        if (imul >= 0) 
         {
             System.out.println("Product is " + rmul + " + " + imul + "i");
-        }
-        else
+        } 
+        else 
         {
             System.out.println("Product is " + rmul + " - " + (-imul) + "i");
         }
     }
 
-    void div()
+    void div() 
     {
         double den = r2 * r2 + i2 * i2;
         double rdiv = (r1 * r2 + i1 * i2) / den;
-        double idiv = (i1 * i2 - r1 * r2) / den;
-        if(idiv >= 0)
+        double idiv = (i1 * r2 - r1 * i2) / den;
+        if (idiv >= 0) 
         {
-            System.out.println("Sum is " + rdiv + " + " + idiv + "i");
-        }
-        else
+            System.out.println("Division is " + rdiv + " + " + idiv + "i");
+        } else 
         {
-            System.out.println("Sum is " + rdiv + " - " + (-idiv) + "i");
+            System.out.println("Division is " + rdiv + " - " + (-idiv) + "i");
         }
     }
 }
 
 public class ComplexNoCalcVar2 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
 
-        meth ComplexCalc = new meth(); 
+        Meth ComplexCalc = new Meth();
 
         System.out.println("Enter real and imaginary parts for number 1: ");
         ComplexCalc.r1 = scanner.nextDouble();
