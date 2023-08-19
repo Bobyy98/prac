fa3c6596febbc9ef8c13c7ddbae46ff450bd30f7
Bobyy98 <132492809+Bobyy98@users.noreply.github.com>
2023-08-20 01:03:02 +0530

    Create array scan.c

---
+++ C prog/2D Array 2x3 (user input)/array scan.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main()
+{
+    int arr[2][3];
+    int i, j;
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("Enter value for arr[%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("Two 2D array elements:\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d ", arr[i][j]);
+            if (j == 2)
+            {
+                printf("\n");
+            }
+        }
+    }
+}
