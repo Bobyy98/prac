f61d83dbc8812fed52d23e2f289b0d806f32b287
Bobyy98 <132492809+Bobyy98@users.noreply.github.com>
2023-08-23 23:24:31 +0530

    Create gcdlcmoccmd.java

---
+++ Java prog/GCD & LCM (method outside main class, cmd)/gcdlcmoccmd.java
@@ -0,0 +1,32 @@
+class method 
+{
+    public static int gcd(int a, int b) 
+    {
+        if (b == 0) 
+        {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) 
+    {
+        return (a * b) / gcd(a, b);
+    }
+}
+
+public class gcdlcmoccmd 
+{
+    public static void main(String[] args) 
+    {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int gcdRes = method.gcd(num1, num2);
+        int lcmRes = method.lcm(num1, num2);
+
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcdRes);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcmRes);
+    }
+}
+
