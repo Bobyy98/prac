4174ec3c9d9cfd84db925295e345002a14215cc5
Bobyy98 <132492809+Bobyy98@users.noreply.github.com>
2023-09-03 22:41:37 +0530

    Create PersonAthLazyAbsIn9.java

---
+++ Java prog/Person Athlete, Lazy (Abstraction, Inheritence)/PersonAthLazyAbsIn9.java
@@ -0,0 +1,51 @@
+// Abstract class Person
+abstract class Person {
+    // Abstract method eat()
+    public abstract void eat();
+
+    // Abstract method exercise()
+    public abstract void exercise();
+}
+
+// Subclass Athlete
+class Athlete extends Person {
+    @Override
+    public void eat() {
+        System.out.println("An athlete eats a balanced diet with lots of protein.");
+    }
+
+    @Override
+    public void exercise() {
+        System.out.println("An athlete exercises rigorously, including strength training and cardio workouts.");
+    }
+}
+
+// Subclass LazyPerson
+class LazyPerson extends Person {
+    @Override
+    public void eat() {
+        System.out.println("A lazy person often eats unhealthy fast food and snacks.");
+    }
+
+    @Override
+    public void exercise() {
+        System.out.println("A lazy person rarely exercises and prefers a sedentary lifestyle.");
+    }
+}
+
+public class PersonAthLazyAbsIn9 {
+    public static void main(String[] args) {
+        // Create an Athlete and a LazyPerson
+        Athlete athlete = new Athlete();
+        LazyPerson lazyPerson = new LazyPerson();
+
+        // Describe how each person eats and exercises
+        System.out.println("Athlete:");
+        athlete.eat();
+        athlete.exercise();
+
+        System.out.println("\nLazy Person:");
+        lazyPerson.eat();
+        lazyPerson.exercise();
+    }
+}
