a6e4bd74e1cb18dcfb8f9e0233816ab9d2b3ec05
Bobyy98 <132492809+Bobyy98@users.noreply.github.com>
2023-09-03 20:10:59 +0530

    Create PersonEmployeeInherit4.java

---
+++ Java prog/Person Employee (Inheritance)/PersonEmployeeInherit4.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
+
+class Employee extends Person {
+    private int employeeId;
+    private String jobTitle;
+
+    public Employee(String firstName, String lastName, int employeeId, String jobTitle) {
+        super(firstName, lastName);
+        this.employeeId = employeeId;
+        this.jobTitle = jobTitle;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    @Override
+    public String getLastName() {
+        return super.getLastName() + ", " + jobTitle;
+    }
+}
+
+public class PersonEmployeeInherit4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Enter last name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Enter employee ID: ");
+        int employeeId = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character left in the input buffer
+
+        System.out.print("Enter job title: ");
+        String jobTitle = scanner.nextLine();
+
+        Employee employee = new Employee(firstName, lastName, employeeId, jobTitle);
+
+        System.out.println("First Name: " + employee.getFirstName());
+        System.out.println("Last Name: " + employee.getLastName());
+        System.out.println("Employee ID: " + employee.getEmployeeId());
+
+        scanner.close();
+    }
+}
